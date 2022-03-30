1331a6e759bd3b5ed43a0a42719c4af4976d8db0
Lean97-start <Leandro_mza008@hotmail.com>
2022-03-30 10:24:36 -0300

    Falta Modularizar back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -16,13 +16,10 @@ async function addType(array){
     await Promise.all(array.map(type => {Type_diet.findOrCreate({where: {name: type}})}))
 }
 
-
-
 router.get('/recipes', async (req, res) => { //FUNCIONA
-    //Voy a buscar por el name a la base de datos. Luego busco en los 100 resultados que me salen y los concateno al ser un array. Despues de eso, agarro y uso un filter para buscar que contengan ese name. 
     const name = req.query.name; //Obtengo el nombre de la receta que me viene por query.
     try{
-        const searchname_BD = await Recipe.findAll({include:[{model: Type_diet}]});
+        const searchname_BD = await Recipe.findAll({include:[{model: Type_diet}]}); //Search a la base de datos de todas las recetas
         const searchname_API = await axios('https://api.spoonacular.com/recipes/complexSearch?apiKey=1323a609c8b8420aba666bd2d26f2fb8&addRecipeInformation=true&number=100');
         const infoAPI = await searchname_API.data.results.map(recipe => {
             addType(recipe.diets)
@@ -40,13 +37,11 @@ router.get('/recipes', async (req, res) => { //FUNCIONA
             }
         });
         const recipes = infoAPI.concat(searchname_BD);
-        const filterRecipe = recipes.filter(recipe => recipe.name.includes(name));
+        if(!name) res.status(200).json(recipes); //Si no me viene ningun nombre por query, que devuelva todas las recetas.
+        const filterRecipe = recipes.filter(recipe => recipe.name.toUpperCase().includes(name.toUpperCase())); //Uppercase porque sino puede que no encuentre coincidencias.
         (filterRecipe.length > 0)? res.status(200).json(filterRecipe) : res.status(404).json({msg: "No se ha encontrado ninguna receta con ese nombre."});
     }catch(e){console.log(e)}
 })
-
-
-
 
 router.get("/recipes/:id", async (req, res) => { //FUNCIONA
     const id_rec = req.params.id; //Obtengo el id que me viene por params
@@ -56,48 +51,42 @@ router.get("/recipes/:id", async (req, res) => { //FUNCIONA
     let valor;
     if (regexAPI.test(id_rec)) {
         try{
-        valor = await axios(`https://api.spoonacular.com/recipes/${id_rec}/information?apiKey=1323a609c8b8420aba666bd2d26f2fb8`);
-        let rec = valor.data;
-        
-        return res.json({
-            id: rec.id,
-            name: rec.title,
-            summary: rec.summary,
-            readyInMinutes: rec.readyInMinutes,
-            score: rec.weightWatcherSmartPoints,
-            healthScore: rec.healthScore,
-            dishTypes: rec.dishTypes,
-            step_by_step: rec.instructions,
-            image: rec.image,
-            type_diets: rec.diets,
-        });
+            valor = await axios(`https://api.spoonacular.com/recipes/${id_rec}/information?apiKey=1323a609c8b8420aba666bd2d26f2fb8`);
+            let rec = valor.data;
+            return res.status(200).json({
+                id: rec.id,
+                name: rec.title,
+                summary: rec.summary,
+                readyInMinutes: rec.readyInMinutes,
+                score: rec.weightWatcherSmartPoints,
+                healthScore: rec.healthScore,
+                dishTypes: rec.dishTypes,
+                step_by_step: rec.instructions,
+                image: rec.image,
+                type_diets: rec.diets,
+            });
         }catch(e){return error_response()}
     } else if(regexBD.test(id_rec)){
         try{
-        valor = await Recipe.findByPk(id_rec, {include: {model: Type_diet, attributes: ['name'], through: {attributes: []}}});
-        //through me garantiza que solo me va a traer esos atributos que le pongo en attributes.
-        if(!valor) return error_response()
-        return res.send(valor);
-        }catch(e){console.log(error_response())}
+            valor = await Recipe.findByPk(id_rec, {include: {model: Type_diet, attributes: ['name'], through: {attributes: []}}});
+            //through me garantiza que solo me va a traer esos atributos que le pongo en attributes.
+            if(!valor) return error_response()
+            return res.send(valor);
+        }catch(e){console.log(e)}
     }else{
         return error_response()
     }
-  });
+});
 
-
-
-
-
-router.get('/types', async (req, res) =>{ //FUNCIONA
+router.get('/types', (req, res) =>{ //FUNCIONA
     const types_diet = ["omnivore", "pescetarian", "vegetarian", "vegan", "fruitarian", "paleo"];
 
-    //Ejecuto un map en el cual me va a ejecutar por promesas la búsqueda a la BD, sino está el tipo de dieta, lo crea.
-    await Promise.all(types_diet.map(type => {Type_diet.findOrCreate({where: {name: type}})}))
+    //Ejecuto esta función la cual me va a ejecutar por promesas la búsqueda a la BD, sino está el tipo de dieta, lo crea.
+    addType(types_diet)
     //Busco todos los tipos de datos para devolverlo en formato Json.
     .then(type => Type_diet.findAll())
     //En caso de que no se cumpla la promesa, se ejecutara la parte reject.
-    .then(response => {res.send(response)}, error => res.status(404).json({msg: "No se encontraron los tipos de dietas"}))
-    .catch(e=> console.log(e))
+    .then(response => {res.send(response)}, error => res.status(404).json({msg: "No se encontraron los tipos de dietas"}));
 
     //Podría hacerlo con Asyc Await:
     // try{
@@ -111,10 +100,6 @@ router.get('/types', async (req, res) =>{ //FUNCIONA
 
 })
 
-
-
-
-
 router.post('/recipe', async (req, res) =>{ //FUNCIONA
     const {name, summary, score, healthScore, step_by_step, image, readyInMinutes, id_type_diet} = req.body; 
     if(!name) return res.status(400).json({msg: "No ingreso ningun nombre para la receta"});
@@ -127,7 +112,7 @@ router.post('/recipe', async (req, res) =>{ //FUNCIONA
         return res.status(500).json({msg: "Ha ocurrido un error en la creación de la receta, inténtelo de nuevo"})
     }
     // return res.redirect(`/recipes/${recipe_created.id}`);
-    return res.status(200).json({msg: "debe redireccionarme a la pág de la receta que se creo."});
+    return res.status(200).json({msg: "Receta creada correctamente."});
 })
 
 module.exports = router;
