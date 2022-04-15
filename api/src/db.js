02f7ef07b66210aa600925cab305fa3bde260794
Lean97-start <Leandro_mza008@hotmail.com>
2022-04-15 15:59:37 -0300

    Deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
-
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
